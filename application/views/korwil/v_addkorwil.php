4f3ebacae2c017761caaf9c01625bf9fb2caf1df
alieffebrina <alief.febrina@gmail.com>
2020-09-21 14:13:06 +0700

    Update v_addkorwil.php

--- application/views/korwil/v_addkorwil.php
+++ application/views/korwil/v_addkorwil.php
@@ -85,6 +85,15 @@
                         </span>
                   </div>
                   </div>
+                </div>
+<div class='row'>
+                  <div class='col-lg-12'>
+                    <label for="inputEmail3" class="col-sm-2 control-label"></label>
+                    <div class="col-sm-3">
+                      <button type="submit" class="btn btn-primary">Simpan</button>
+                      <a href="<?php echo site_url('Korwil'); ?>" class="btn btn-default">Batal</a>
+                    </div>
+                  </div>
                 </div> 
               </div>
           </div>
