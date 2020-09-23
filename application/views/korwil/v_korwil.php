83c5bd3467fbb01b21f175365a739afae737d13b
alieffebrina <alief.febrina@gmail.com>
2020-09-23 07:59:54 +0700

    Update v_korwil.php

--- application/views/korwil/v_korwil.php
+++ application/views/korwil/v_korwil.php
@@ -27,7 +27,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Data Korwil</h3>
+              <h3 class="box-title">Data Cabang / Wilayah</h3>
             </div>
 
             <div class="box-header">
