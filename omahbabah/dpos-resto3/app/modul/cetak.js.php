49f1bba838fd7e8025d9b84acb569ebb596f9e2c
alieffebrina <alief.febrina@gmail.com>
2020-10-01 20:18:04 +0700

    hapus templte omahbaba

--- omahbabah/dpos-resto3/app/modul/cetak.js.php
+++
@@ -1,8 +0,0 @@
-<script src="<?php echo $CORE_URL;?>/assets/plugins/air-datepicker/js/datepicker.min.js"></script>
-<script src="<?php echo $CORE_URL;?>/assets/plugins/air-datepicker/js/i18n/datepicker.id.js"></script>
-<script>
-$(document).ready(function() {
-	
-	
-});
-</script>
